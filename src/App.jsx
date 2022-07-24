269e953b012fd38676eefdb710764163504dff86
Elias-Andrade-Dos-Santos <elielias351@gmail.com>
2022-07-23 22:57:35 -0300

    carrosel add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Home'
+import Carrosel from './components/carrosel'
 
 const App = () => {
   return (
     <div>
       <Home/>
+      <Carrosel/>
     </div>
   )
 }
---
+++ src/components/carrosel.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Slider from "react-slick";
+import styles from "../css/carrosel.module.css"
+import Nodejs from "../img/svg/nodejs.svg"
+
+
+
+const carrosel = () => {
+    var settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+    }
+
+  return (
+    <div>
+        <div>
+            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        </div>
+        <div className={styles.barTop}></div>
+        <div className={styles.superContainer}>
+            <h1>TECNOLOGIAS</h1>
+            <Slider {...settings} className={styles.container}>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            <div>
+                <img src={Nodejs} alt="" />
+            </div>
+            </Slider>
+        </div>
+        <div className={styles.barBottom}></div>
+    </div>
+  )
+}
+
+export default carrosel
