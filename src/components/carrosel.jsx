15826d92b292baf0661485f23040a1f8bbe644c5
Elias-Andrade-Dos-Santos <elielias351@gmail.com>
2022-07-26 21:01:41 -0300

    Subindo

--- src/components/carrosel.jsx
+++ src/components/carrosel.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Slider from "react-slick";
 import styles from "../css/carrosel.module.css"
 import Nodejs from "../img/svg/nodejs.svg"
-
+import Reactjs from "../img/svg/reactjs.svg"
+import Js from '../img/svg/js.svg'
+import Php from "../img/svg/php-svgrepo-com.svg"
+import Mysql from "../img/svg/mysql-svgrepo-com.svg"
+import Mongo from "../img/svg/mongodb-svgrepo-com.svg"
 
 
 const carrosel = () => {
@@ -30,19 +34,19 @@ const carrosel = () => {
                 <img src={Nodejs} alt="" />
             </div>
             <div>
-                <img src={Nodejs} alt="" />
+                <img src={Reactjs} alt="" />
             </div>
             <div>
-                <img src={Nodejs} alt="" />
+                <img src={Js} alt="" />
             </div>
             <div>
-                <img src={Nodejs} alt="" />
+                <img src={Php} alt="" />
             </div>
             <div>
-                <img src={Nodejs} alt="" />
+                <img src={Mysql} alt="" />
             </div>
-            <div>
-                <img src={Nodejs} alt="" />
+            <div className={styles.logoMongo}>
+                <img src={Mongo} alt="" />
             </div>
             </Slider>
         </div>
